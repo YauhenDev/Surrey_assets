0b106e0f5336dd3d3715e343c16f42cb2ba665ee
YauhenDev <buchenkov@gmail.com>
2022-01-27 11:21:04 +0300

    update SVG

--- components/ui/logo/LogoTxtSvg.jsx
+++ components/ui/logo/LogoTxtSvg.jsx
@@ -13,55 +13,74 @@ export default function LogoTxtSvg({
 	const yAssets = ySurrey
 
 	// MOBILE
-	// const svgBox = "0 0 90 40"
-	// const xSurrey = 3
-	// const ySurrey = 16
-	// const xAssets = 6
-	// const yAssets = 33
+	const svgBoxMobile = "0 0 90 40"
+	const xSurreyMobile  = 3
+	const ySurreyMobile  = 16
+	const xAssetsMobile  = 6
+	const yAssetsMobile  = 33
 
-
-	// const [isMobile, setIsMobile] = useState(widthDevice);
-	// const ismobile = widthDevice < 576;
-	// if (ismobile !== isMobile) setIsMobile(ismobile);
-
-	// useEffect(() => {
-	// 	function tglMenu() {
-	// 		if (ismobile) {
-	// 			console.log("mobile")
-		
-	// 			const svgBox = "0 0 90 40"
-	// 			const xSurrey = 3
-	// 			const ySurrey = 16
-	// 			const xAssets = 6
-	// 			const yAssets = 33
-	// 		} else {
-	// 			console.log("pc")
-		
-	// 			const svgBox = "0 0 163 19"
-	// 			const xSurrey = 0
-	// 			const ySurrey = 16
-	// 			const xAssets = 90
-	// 			const yAssets = ySurrey
-	// 		}
-	// 	}
-	// }, []);
-	
+	const [isMobile, setIsMobile] = useState(widthDevice);
+	const ismobile = widthDevice < 576;
+	if (ismobile !== isMobile) setIsMobile(ismobile);
 
 	//debugger
 	return (
-	<svg className={ styles.intro } viewBox={ svgBox }>
-		<text textAnchor="start" x={ xSurrey } y={ ySurrey } className={ styles.textStroke } clipPath="url(#surrey)">SURREY</text>
-		<text textAnchor="start" x={ xAssets } y={ yAssets } className={ styles.textStroke } clipPath="url(#assets)">ASSETS</text>
+	<svg className={ styles.intro } viewBox={isMobile ? svgBoxMobile : svgBox }>
+		<text 
+			textAnchor="start" 
+			x={isMobile ? xSurreyMobile : xSurrey }
+			y={isMobile ? ySurreyMobile : ySurrey }
+			className={ styles.textStroke } 
+			clipPath="url(#surrey)">
+				SURREY
+		</text>
+		<text 
+			textAnchor="start" 
+			x={isMobile ? xAssetsMobile : xAssets }
+			y={isMobile ? yAssetsMobile : yAssets }
+			className={ styles.textStroke } 
+			clipPath="url(#assets)"
+		>
+			ASSETS
+		</text>
 		
-		<text textAnchor="start" x={ xSurrey } y={ ySurrey } className={`${ styles.textStroke } ${ styles.textStrokeNext }`} clipPath="url(#surrey)">SURREY</text>
-		<text textAnchor="start" x={ xAssets } y={ yAssets } className={`${ styles.textStroke } ${ styles.textStrokeNext }`} clipPath="url(#assets)">ASSETS</text>
+		<text 
+			textAnchor="start" 
+			x={isMobile ? xSurreyMobile : xSurrey }
+			y={isMobile ? ySurreyMobile : ySurrey }
+			className={`${ styles.textStroke } ${ styles.textStrokeNext }`} 
+			clipPath="url(#surrey)"
+		>
+			SURREY
+		</text>
+		<text 
+			textAnchor="start" 
+			x={isMobile ? xAssetsMobile : xAssets }
+			y={isMobile ? yAssetsMobile : yAssets }
+			className={`${ styles.textStroke } ${ styles.textStrokeNext }`} 
+			clipPath="url(#assets)"
+		>
+			ASSETS
+		</text>
 		
 		<defs>
 			<clipPath id="surrey">
-				<text textAnchor="start" x={ xSurrey } y={ ySurrey }>SURREY</text>
+				<text 
+					textAnchor="start" 
+					x={isMobile ? xSurreyMobile : xSurrey }
+					y={isMobile ? ySurreyMobile : ySurrey }
+				>
+					SURREY
+				</text>
 			</clipPath>
 			<clipPath id="assets">
-				<text textAnchor="start" x={ xAssets } y={ yAssets }>ASSETS</text>
+				<text 
+					textAnchor="start" 
+					x={isMobile ? xAssetsMobile : xAssets }
+					y={isMobile ? yAssetsMobile : yAssets }
+				>
+					ASSETS
+				</text>
 			</clipPath>
 		</defs>
 	</svg>

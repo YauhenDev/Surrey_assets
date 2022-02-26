7d6c4bd663f8c66b98aff1bff195d784611c62ec
YauhenDev <buchenkov@gmail.com>
2022-02-27 00:28:52 +0300

    add ru

--- locales/ru.js
+++ locales/ru.js
@@ -49,6 +49,14 @@ export const ru = {
 			invest		: false,
 		},
 		{	
+			nameLink	: "Our Team",
+			titleLink	: "Our Team",
+			seoTitle	: "Our Team",
+			seoDescription : "",
+			pathLink	: "/our-team",
+			invest		: false,
+		},
+		{	
 			nameLink	: "Наши услуги",
 			titleLink	: "Наши услуги",
 			seoTitle	: "Наши услуги",
@@ -66,32 +74,45 @@ export const ru = {
 		}
 	],
 	aboutUs: {
-		h2		: "SURREY ASSETS LTD",
-		txt1	: "Surrey Assets Ltd. is a European holding company of the combined financial group with stable ties in large financial and industrial structures, investment banks and funds. Our group provides financial consulting, investment and asset management solutions for large institutions, families, and individuals, as well as M&A transactions.",
-		txt2	: "We offer participation in investment programs in the implementation of large infrastructure projects in various sectors of the economy, in the implementation of Development projects, Agriculture, Oil and Gas sector, Mining and Real Estate, among others.",
-		txt3	: "We provide a discreet alternative service to the UHNWI and international business operators who wish to source prime investment opportunities and new global markets – to either acquire, or dispose of Assets, whilst providing a wide range of complementary services in order to protect those assets and the individual, observing strict levels of confidentiality at all times. Over the years, the Surrey Assets Ltd have built up a global network of high-level contacts, ensuring that we place no limits on our spheres of operation, nor indeed the scope of our operational capability. ",	
-		txtFull	: "Subdivisions and partners of our group are present in the Netherlands, France, Austria, Italy, India, Germany, the United States of America, Brazil, China, Africa.",	
+		h2		: "Vision Statement",
+		txt1	: "<ul><li>Customer-Oriented: achieving mutually beneficial relationships with our customers through long-lasting business experience</li><li>Committed to Growth: hand-picking and training the best and most experienced team in the business, able to master organic personal and company growth while maintaining contract-to-payment full control, providing employees a fulfilling work environment</li></ul>",
+		txt2	: "<strong>Our Strengths</strong><br />We strive to provide our customers with a comprehensive range of services and a quality of product that fully meets or exceeds their requirements and expectations.",
+		txt3	: "<p>We help our customers in structuring transactions, finding reasonable approaches to business development, advising on the choice of more promising areas for investment, and developing solutions to ensure the safety of capital.</p><p>Our strategy is to always look at the big picture and explore all the areas and options available. As an independent player, we offer expert advice and business models that are simple and honest.</p>",	
+		txtFull	: "<p>We act independently, with the highest level of integrity and expect to become leaders in our markets.</p><ul><li>Independent company with the highest level of integrity</li><li>Long-term relationships in the industry</li><li>Multilingual team, we speak a dozen languages</li><li>Competitive bids and offers</li><li>Pricing and hedging solutions available based on LME</li><li>Spot and long-term deals</li><li>Regulatory compliance, safety first</li></ul>",	
 	},
 	ourServices: {
 		h2		: "SURREY ASSETS LTD",
+		txt		: "<p>Our team of specialists will always provide clients with specialized services for managing equity investments, fixed income, and diversified capital management. The European office of our company strives to balance the investment portfolio of our clients, finding rational approaches to reduce risks and increase profitability. We are always ready to advise clients for their further effective investments.</p><p>Our company always offers asset management services to institutional market participants, external distributors and financial consultants. Our full range of products includes stocks, fixed income bonds, convertible bonds, and diversified funds.</p>",
 		services: [
 			{
-				anchor: "banking",
-				h3: 	"Banking instruments",
+				anchor: "fund",
+				h3: 	"Wealth management",
 				img: 	"banking.jpg",
-				txt: 	"<p>BG/SBLC (CASH BACKED, FRESH CUT, FORMAT 758 URDG, ICC 600) for Purchases and for Leasing Trades in a partnership.</p><p>We have direct contacts with Fund Owners and Providers/Principals, so we have the best offers and prices. We have the opportunity together with our friendly banks to issue various banking instruments for clients according to their request (LOI) and project needs.</p>",
+				txt: 	"<p>We provide a wide range of wealth management services to successful families, entrepreneurs, foundations, and charities. We advise our clients on all their financial and non-financial wealth. By helping our clients invest, we strive to ensure good returns and reduce risks even in the most difficult financial conditions. We view investment as an art and are very careful about our clients ' wishes, providing advice for them. Investment structuring and asset protection in order to preserve and increase the capital of our clients are the main tasks that we set for ourselves. The long-term value of any investment must be reasonable and efficient. This is possible only if we have a guaranteed partner who we want to become for our clients.</p>",
+			},
+			{
+				anchor: "finance",
+				h3: 	"Trade and export Finance",
+				img: 	"fin.jpg",
+				txt: 	"<p>We recognize that trade finance can be challenging for clients who are more focused on their manufacturing, commodity and other lines of business. In turn, our specialists also work with designated brokers and representatives of these clients to best arrive at a dynamic solution.</p><p>Ultimately, our company always maintains a customer-oriented vision to come and provide the best solution. We can help your business consider a wide range of financing options, from traditional overdraft or bank loans to more innovative crowdfunding or alternative financing. We understand that trade finance clients come with different needs.</p><p>The banking instruments that clients require can be differentiated by the size of the instrument, the duration of the instrument, the payment structure, the rating of the issuing bank or advising bank, and the relationship the client maintains with their banks or other financial institutions.</p><p><strong>Banking instruments</strong></p><ul><li>BG</li><li>SBLC (CASH BACKED, FRESH CUT, FORMAT 758 URDG, ICC 600) for Purchases and for Leasing Trades in a partnership.</li></ul><p>We have direct contacts with Fund Owners and Providers/Principals, so we have the best offers and prices. We have the opportunity together with our friendly banks to issue various banking instruments for clients according to their request (LOI) and project needs.</p><p>We help you connect with key influencers and stakeholders across many different financial institutions to arrange trade finance for our clients as quickly as possible.</p>",
 			},
 			{
 				anchor: "oil-and-gas",
 				h3: 	"Oil and Gas sector and Mining industry",
 				img: 	"oil.jpg",
-				txt: 	"<p>Deliveries of diesel fuel, natural gas, urea, gold, copper, etc.</p>",
+				txt: 	"<p>Our oil & gas team helps clients meet the complex trade challenges they face</p><p>Our oil & gas trading team has many years of experience working with government owned producers, leading oil traders and energy multinationals. This has given us critical insight into how to put together deals, assess risks and advise on trading issues and the financing, transport and supply of oil and gas.</p>",
+			},
+			{
+				anchor: "metals",
+				h3: 	"Refined and Precious Metals",
+				img: 	"metal.jpg",
+				txt: 	"<p>We offer trading in bullion, bars, and coins in gold and silver. When trading precious metals, Surrey Assets acts as an agent only; we hold no inventory in refined and precious metals, nor does Surrey Assets buy or sell by taking positions in the market. We send your orders directly to the refined and precious metals plants or investment companies we use who buy and sell for us in the precious metals marketplace.</p>",
 			},
 			{
 				anchor: "development",
-				h3: 	"Development sector",
+				h3: 	"Real estate development and investment",
 				img: 	"dev.jpg",
-				txt: 	"<p>Investment in Residential and Commercial Real Estate in Italy, Germany, France, Switzerland, Russia</p>",
+				txt: 	"<p>Real estate development and investment covers a range of real estate specialists, our team can provide you with commercial and legal advice on all aspects of residential and commercial sites.</p><p><strong>Real estate funds</strong><p>We act for real estate investment funds and their managers regarding their investment activities and have a comprehensive understanding of the investment structures typically used. Our experts advise on the divestment of indirect holdings and formulate work-out plans in relation to existing investments, supporting you in secondary sales of fund interests or in the pursuit of other realization opportunities. Our funds team works with our wider taxation and corporate teams, so you are supported by a full team committed to your investment aims in Italy, Germany, France, Switzerland, Russia and other countries.</p>",
 			},
 		]
 	},
@@ -169,6 +190,7 @@ export const ru = {
 	},
 	contacts: {
 		h2		: "Связаться с нами",
+		txt		: "We are at your service and will respond to your request as soon as possible",
 		methods: [
 			{
 				ico		: "envelope",
@@ -180,21 +202,21 @@ export const ru = {
 				link	: "https://wa.me/70001234567",
 				alt		: "Чат в WhatsApp",
 			},
-			{
+			/* {
 				ico		: "viber",
 				link	: "viber://chat?number=+70001234567",
 				alt		: "Чат в Viber",
-			},
+			}, */
 			{
 				ico		: "telegram-plane",
 				link	: "https://telegram.me/SurreyAssets",
 				alt		: "Чат в Telegram",
 			},
-			{
+			/* {
 				ico		: "facebook-messenger",
 				link	: "https://m.me/SurreyAssets",
 				alt		: "Чат в messenger",
-			},
+			}, */
 			{
 				ico		: "phone-alt",
 				link	: "tel:+70001234567",
